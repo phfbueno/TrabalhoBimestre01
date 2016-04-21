9c451aaccee759d38825a40949ebee8f612099f7
phfbueno <phf.bueno@gmail.com>
2016-04-20 22:13:50 -0300

    Implentando a classe cliente, criando os atributos

--- src/Trabbim01/Cliente.java
+++ src/Trabbim01/Cliente.java
@@ -1,5 +1,13 @@
 package Trabbim01;
 
+@Tabela("Cad_cliente")
 public class Cliente {
+	@Coluna(pk = true)
+	private int id; 
+	@Coluna(nome = "CLNOME")
+	private String nome;
+	@Coluna(nome = "CLEND")
+	private String endereco;
+
 
 }
